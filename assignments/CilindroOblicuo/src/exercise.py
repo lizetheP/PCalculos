50ce3664376650b1f3dbc03747a4f205ea32ed96
Lizethe <lizetheperez@gmail.com>
2021-07-30 06:18:42 -0500

    subir

--- assignments/CilindroOblicuo/src/exercise.py
+++ assignments/CilindroOblicuo/src/exercise.py
@@ -1,11 +1,5 @@
-import math
-
 def main():
-    r = float(input("Radio: "))
-    h1 = float(input("Altura 1: "))
-    h2 = float(input("Altura 2: "))
-    v = (math.pi*math.pow(r,2)*(h1+h2))/2 
-    print("Volumen: %.2f" % v)
+    #escribe tu código abajo de esta línea
 
-if __name__ == '__main__':
+if __name__=='__main__':
     main()
--- assignments/Tiempo/src/exercise.py
+++ assignments/Tiempo/src/exercise.py
@@ -1,13 +1,5 @@
 def main():
-    tiempo = int(input("Introduce el tiempo: "))
-    horas = tiempo // 3600
-    print("Horas:", horas)
-    residuo = tiempo % 3600
-    #print("Residuo :", residuo)
-    minutos = residuo // 60
-    print("Minutos:", minutos)
-    segundos = residuo % 60
-    print("Segundos:", segundos)
+    #escribe tu código abajo de esta línea
 
-if __name__ == '__main__':
+if __name__=='__main__':
     main()
